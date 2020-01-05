406242561b7fba6060a620aa2b9cb5dc01a9af61
Fengfeng WANG <fengfeng.wang@valeo.com>
2020-01-05 10:45:24 +0800

    Practice basic data type

---
+++ src/002_Basics.py
@@ -0,0 +1,34 @@
+#This is comment
+
+#declare variables
+birthMonth=3
+print(type(birthMonth))
+print(birthMonth,'\n')
+
+currentTemp=20.7
+print(type(currentTemp))
+print(currentTemp,'\n')
+
+currentPoint=1+2j
+print(type(currentPoint))
+print(currentPoint,'\n')
+
+myName="Lucky"
+print(type(myName))
+print(myName,'\n')
+
+seasonOne=['Jan','Feb','March']
+print(type(seasonOne))
+print(seasonOne,'\n')
+
+seasonOne=(1,2,3)
+print(type(seasonOne))
+print(seasonOne,'\n')
+
+seasonOne={'Jan',"Feb",'Mar'}
+print(type(seasonOne))
+print(seasonOne,'\n')
+
+seasonOne={'jan':1,'Feb':2,"Mar":3}
+print(type(seasonOne))
+print(seasonOne,'\n')
